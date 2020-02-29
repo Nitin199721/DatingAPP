0b78c2dbc7a29c63facb0a0ec732566cc03b84e7
Nitin Singhal <2247@xceedance.com>
2020-02-29 19:01:13 +0530

    End of RegisterandLogin

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace DatingApp.API.Controllers
 {
-    
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -21,7 +22,7 @@ namespace DatingApp.API.Controllers
             _context = context;
 
         }
-
+         [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
@@ -29,7 +30,7 @@ namespace DatingApp.API.Controllers
             return Ok(values);
         }
 
-        
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
