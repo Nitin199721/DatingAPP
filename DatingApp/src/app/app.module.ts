25bb0c2664ca238ad38acc99e01d9987fdec3619
Nitin Singhal <2247@xceedance.com>
2020-03-01 00:18:45 +0530

    End of Section 6

--- DatingApp/src/app/app.module.ts
+++ DatingApp/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import {FormsModule} from '@angular/forms';
-import { from } from 'rxjs';
+import { BsDropdownModule } from 'ngx-bootstrap';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
+
 
 @NgModule({
    declarations: [
@@ -22,11 +24,13 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
-      ErrorInterceptorProvider
+      ErrorInterceptorProvider,
+      AlertifyService
    ],
    bootstrap: [
       AppComponent
