d825068756290b34cb09f5459fd1b718026d9bcf
Nitin Singhal <2247@xceedance.com>
2020-03-04 18:26:47 +0530

    End Of Section 10

--- DatingApp/src/app/app-routing.module.ts
+++ DatingApp/src/app/app-routing.module.ts
@@ -8,6 +8,9 @@ import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolver/member-detail.resolver';
 import { MemberListResolver } from './_resolver/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolver/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 
 const routes: Routes = [
@@ -19,6 +22,8 @@ const routes: Routes = [
     children: [
       {path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
       {path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
+      {path: 'member/edit', component: MemberEditComponent, resolve: {user: MemberEditResolver},
+    canDeactivate: [PreventUnsavedChanges]},
       {path: 'messages', component: MessagesComponent},
       {path: 'lists', component: ListsComponent},
     ]
